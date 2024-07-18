f859337775d036b8dfdeb2edeee576edf54859db
blandygbc <gabriel_blandy@hotmail.com>
2024-07-17 21:17:14 -0300

    Finish spring security with database

--- src/main/kotlin/com/blandygbc/firstboot/backend/infra/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/blandygbc/firstboot/backend/infra/security/SecurityConfiguration.kt
@@ -4,27 +4,28 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import javax.sql.DataSource
 
 @Configuration
 class SecurityConfiguration {
 
     @Bean
-    fun userDetailsService(): UserDetailsService {
-        val user = User.builder()
-            .username("user")
-            .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
-            .roles("USER")
-            .build()
-        val admin = User.builder()
-            .username("admin")
-            .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
-            .roles("USER", "ADMIN")
-            .build()
-        return InMemoryUserDetailsManager(user, admin)
+    fun userDetailsService(dataSource: DataSource): UserDetailsService {
+        return JdbcUserDetailsManager(dataSource)
+//        val user = User.builder()
+//            .username("user")
+//            .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+//            .roles("USER")
+//            .build()
+//        val admin = User.builder()
+//            .username("admin")
+//            .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+//            .roles("USER", "ADMIN")
+//            .build()
+//        return InMemoryUserDetailsManager(user, admin)
     }
 
 
